2cac502699851fec174242aefb7cb883241b7657
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 09:09:23 -0400

    Add files via upload

---
+++ Aggregation/Revising Aggregations - SUM.sql
@@ -0,0 +1,12 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+*/
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
+
+/*
+Expected Output
+339002 
+*/
