237b8b6324d74c72339a8ecb0dd240734f374a0c
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 09:14:40 -0400

    Add files via upload

---
+++ Aggregation/Revising Aggregations - AVG.sql
@@ -0,0 +1,12 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+*/
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
+
+/*
+Expected Output
+113000.667
+*/
