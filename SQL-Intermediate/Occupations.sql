0385a143aee0279aaa6498955c6f1dd955dcc58c
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-22 21:32:02 -0400

    Update Occupations.sql

--- SQL-Intermediate/Occupations.sql
+++ SQL-Intermediate/Occupations.sql
@@ -1,3 +1,10 @@
+/*
+ADVANCED SELECT --- Difficulty: Medium (SQL Intermediate)
+Problem Statement: PIVOT
+
+https://www.hackerrank.com/challenges/occupations/problem?isFullScreen=true
+*/
+
 SELECT
     [Doctor], [Professor], [Singer], [Actor]
 FROM
@@ -8,3 +15,17 @@ PIVOT
 (
     MAX(NAME) FOR OCCUPATION IN ([Doctor], [Professor], [Singer], [Actor])
 ) AS pivotTable
+
+
+/*
+
+Your Output (stdout)
+Aamina Ashley Christeen Eve
+Julia Belvet Jane Jennifer
+Priya Britney Jenny Ketty
+NULL Maria Kristeen Samantha
+NULL Meera NULL NULL
+NULL Naomi NULL NULL
+NULL Priyanka NULL NULL
+
+*/
