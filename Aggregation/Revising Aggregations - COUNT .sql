4f87abbbb585809654428cee2bfd6c310ffa015d
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 08:56:07 -0400

    Add files via upload

---
+++ Aggregation/Revising Aggregations - COUNT .sql
@@ -0,0 +1,12 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+*/
+SELECT COUNT(ID)
+FROM CITY
+WHERE POPULATION > 100000
+
+/*
+Expected Output
+6
+*/
