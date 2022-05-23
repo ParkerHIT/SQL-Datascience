ab3ce542925599a3e47c0cf5a1a9276d45282ae1
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-22 20:49:44 -0400

    Add files via upload

---
+++ SQL-Advanced/Print Prime Numbers(Solution 1).sql
@@ -0,0 +1,17 @@
+/* Prime Number Generation Using a Table*/
+
+DECLARE @row int = 3;
+DECLARE @max int = 1000;
+
+CREATE TABLE prime (n int);
+INSERT INTO prime VALUES (2);
+
+WHILE (@row <= @max)
+BEGIN
+    IF (SELECT count(*) FROM prime WHERE @row % n = 0) = 0
+        INSERT INTO prime VALUES (@row);
+    ELSE
+        SET @row = @row + 1;
+END
+
+SELECT string_agg(n, '&') FROM prime;
