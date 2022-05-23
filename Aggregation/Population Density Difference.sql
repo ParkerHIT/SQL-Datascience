6f96db2b412b644f06eaec7eab736916b0a9fbfe
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 09:21:19 -0400

    Add files via upload

---
+++ Aggregation/Population Density Difference.sql
@@ -0,0 +1,12 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+SELECT MAX(POPULATION)- MIN (POPULATION)
+FROM CITY
+
+
+/*
+Expected Output
+4695354
+*/
