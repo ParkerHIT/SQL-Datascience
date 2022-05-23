6c1f0d97b79b5c23141a5c625b530b5ac918708e
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-22 21:19:41 -0400

    Update New Companies.sql

--- SQL-Intermediate/New Companies.sql
+++ SQL-Intermediate/New Companies.sql
@@ -1,4 +1,14 @@
 /*
+
+ADVANCED SELECT ----Difficulty: Medium (SQL Intermediate)
+
+Problem Statement
+
+https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true
+*/
+
+
+/*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
@@ -10,3 +20,17 @@ SELECT COMPANY_CODE, FOUNDER,
 (SELECT COUNT(DISTINCT EMPLOYEE_CODE) FROM EMPLOYEE WHERE COMPANY_CODE = C.COMPANY_CODE)
 FROM COMPANY C
 ORDER BY COMPANY_CODE;
+
+
+
+/*
+
+ Output (stdout)
+C1 Angela 1 2 5 13
+C10 Earl 1 1 2 3
+C100 Aaron 1 2 4 10
+C11 Robert 1 1 1 1
+C12 Amy 1 2 6 14
+
+(100 ROWS)
+*/
