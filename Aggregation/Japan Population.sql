ce4bf0f7949b7a81edf5e67ed40094865ba7ce15
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 09:18:22 -0400

    Add files via upload

---
+++ Aggregation/Japan Population.sql
@@ -0,0 +1,13 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+
+
+/*
+Expected Output
+879196
+*/
