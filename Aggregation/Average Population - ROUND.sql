d5a080add6b29bf23b96f68060af088ba170424e
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-23 09:16:47 -0400

    Add files via upload

---
+++ Aggregation/Average Population - ROUND.sql
@@ -0,0 +1,12 @@
+/*
+Aggregation --- Difficulty:Easy
+Problem Statement : https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+
+SELECT ROUND(AVG(POPULATION),2)
+FROM CITY
+
+
+/*
+Expected Output
+454250
+*/
