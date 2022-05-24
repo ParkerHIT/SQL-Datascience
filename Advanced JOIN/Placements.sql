0656d83018b66a4a64b90c02ae5de8fe86f94935
Anu P Kumar <35459851+ParkerHIT@users.noreply.github.com>
2022-05-24 17:49:46 -0400

    Add files via upload

---
+++ Advanced JOIN/Placements.sql
@@ -0,0 +1,25 @@
+join -- Difficulty Medium ( SQL Intermediate)
+
+Problem Statement: https://www.hackerrank.com/challenges/placements/problem?isFullScreen=true
+
+SELECT S.NAME FROM STUDENTS S
+    FULL OUTER JOIN FRIENDS F ON S.ID = F.ID
+    FULL OUTER JOIN PACKAGES PF ON F.FRIEND_ID = PF.ID
+    FULL OUTER JOIN PACKAGES P ON S.ID = P.ID
+WHERE PF.SALARY > P.SALARY
+ORDER BY PF.SALARY
+
+/*
+Your Output (stdout)
+Stuart
+Priyanka
+Paige
+Jane
+Julia
+Belvet
+Amina
+Kristeen
+Scarlet
+Priya
+Meera
+*/
